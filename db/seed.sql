b010453402684cbaeefe931a989f068c570424c3
Drew <bridgmand@gmail.com>
2021-01-31 19:40:49 -0500

    inserted basic data into tables

--- db/seed.sql
+++ db/seed.sql
@@ -1,2 +1,10 @@
-INSERT INTO department(dp_name) 
-VALUES('Accounting'),('Sales'),('Marketing')
+USE employeeTracker_db;
+
+INSERT INTO department (dp_name) 
+VALUES ('Accounting'), ('Sales'), ('Marketing'), ('Engineering');
+
+INSERT INTO emp_role (title, salary, department_id)
+VALUES ('Senior', 70000, 1), ('Staff', 50000, 2);
+
+INSERT INTO employee (first_name, last_name, role_id, manager_id)
+VALUE ('Jane', 'Doe', 1, 2)
